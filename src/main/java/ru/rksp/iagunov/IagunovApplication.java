a08be223cc76d8100ae79f4f3ed65499eff1a45e
Yagunov Denis <spb.jagunov.denis@gmail.com>
2025-10-19 16:56:55 +0300

    1 stage. Generation of a service in Java.

---
+++ src/main/java/ru/rksp/iagunov/IagunovApplication.java
@@ -0,0 +1,14 @@
+package ru.rksp.iagunov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IagunovApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IagunovApplication.class, args);
+        System.out.println("Ок");
+    }
+
+}
---
+++ src/test/java/ru/rksp/iagunov/IagunovApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rksp.iagunov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IagunovApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
